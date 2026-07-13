a5a75f6e7750115d131cb12c59442c00052db277
CDBrodie <chris.brodie@gmail.com>
2026-07-13 14:28:36 +0100

    Update WorldChat.cpp
    Update for Linux Compile

--- src/WorldChat.cpp
+++ src/WorldChat.cpp
@@ -1,13 +1,6 @@
-#include <algorithm>
-#include <cctype>
-#include <ranges>
-#include <sstream>
-
 #include "WorldChat.h"
-
 #include "Config.h"
 #include "DatabaseEnv.h"
-#include "SocialMgr.h"
 #include "StringFormat.h"
 #include "WorldSessionMgr.h"
 
@@ -15,79 +8,8 @@ static WC::Config g_wcConfig;
 
 static std::string strToLower(std::string str)
 {
-    std::transform(str.begin(), str.end(), str.begin(), [](unsigned char c)
-    {
-        return static_cast<char>(std::tolower(c));
-    });
-
+    std::transform(str.begin(), str.end(), str.begin(), ::tolower);
     return str;
-}
-
-static std::string TrimString(std::string str)
-{
-    while (!str.empty() && std::isspace(static_cast<unsigned char>(str.front())))
-        str.erase(str.begin());
-
-    while (!str.empty() && std::isspace(static_cast<unsigned char>(str.back())))
-        str.pop_back();
-
-    return str;
-}
-
-static std::vector<std::string> SplitCsv(std::string const& input)
-{
-    std::vector<std::string> result;
-    std::stringstream ss(input);
-    std::string token;
-
-    while (std::getline(ss, token, ','))
-    {
-        token = strToLower(TrimString(token));
-        if (!token.empty())
-            result.push_back(token);
-    }
-
-    return result;
-}
-
-static std::string NormalizeMessage(std::string msg)
-{
-    msg = strToLower(msg);
-
-    std::string normalized;
-    normalized.reserve(msg.size());
-
-    bool lastWasSpace = false;
-
-    for (char c : msg)
-    {
-        unsigned char uc = static_cast<unsigned char>(c);
-
-        if (std::isalnum(uc))
-        {
-            normalized += static_cast<char>(uc);
-            lastWasSpace = false;
-        }
-        else if (std::isspace(uc))
-        {
-            if (!lastWasSpace)
-            {
-                normalized += ' ';
-                lastWasSpace = true;
-            }
-        }
-    }
-
-    return TrimString(normalized);
-}
-
-static bool ContainsAny(std::string const& lowerMsg, std::initializer_list<char const*> terms)
-{
-    for (char const* term : terms)
-        if (lowerMsg.find(term) != std::string::npos)
-            return true;
-
-    return false;
 }
 
 static WC::PlayerState& GetState(Player const* player)
@@ -113,13 +35,8 @@ static void LoadPlayerState(Player* player)
 
     state.receiveEnabled = g_wcConfig.loginState;
     state.worldMode = false;
-
     state.muteUntil = 0;
     state.recentMessages.clear();
-
-    state.lastNormalizedMessage.clear();
-    state.lastDuplicateTime = 0;
-    state.duplicateCount = 0;
 
     if (QueryResult result = CharacterDatabase.Query(
         "SELECT `receive_enabled`, `world_mode` FROM `character_worldchat` WHERE `guid` = {} LIMIT 1",
@@ -181,134 +98,6 @@ static bool IsFlooding(Player const& sender)
     return false;
 }
 
-static bool IsDuplicateMessage(Player const& sender, std::string const& msg)
-{
-    if (!g_wcConfig.duplicateDetection)
-        return false;
-
-    if (sender.GetSession()->GetSecurity() > SEC_PLAYER)
-        return false;
-
-    std::string normalized = NormalizeMessage(msg);
-
-    if (normalized.empty())
-        return false;
-
-    WC::PlayerState& state = GetState(&sender);
-    time_t now = time(nullptr);
-
-    if (state.lastNormalizedMessage == normalized &&
-        now - state.lastDuplicateTime <= time_t(g_wcConfig.duplicateWindowSeconds))
-    {
-        ++state.duplicateCount;
-    }
-    else
-    {
-        state.lastNormalizedMessage = normalized;
-        state.lastDuplicateTime = now;
-        state.duplicateCount = 1;
-    }
-
-    if (state.duplicateCount > g_wcConfig.duplicateMaxRepeats)
-    {
-        ChatHandler(sender.GetSession()).PSendSysMessage(
-            "{}[World]|r {}Please avoid repeating the same message.|r",
-            WC::ChatColor::WORLD,
-            WC::ChatColor::YELLOW
-        );
-        return true;
-    }
-
-    return false;
-}
-
-static bool ContainsBlockedUrl(Player const& sender, std::string const& msg)
-{
-    if (!g_wcConfig.urlFilter)
-        return false;
-
-    if (sender.GetSession()->GetSecurity() > SEC_PLAYER)
-        return false;
-
-    std::string lowerMsg = strToLower(msg);
-
-    for (std::string const& term : g_wcConfig.blockedUrlTerms)
-    {
-        if (!term.empty() && lowerMsg.find(term) != std::string::npos)
-        {
-            ChatHandler(sender.GetSession()).PSendSysMessage(
-                "{}[World]|r {}Links and advertising are not allowed in World Chat.|r",
-                WC::ChatColor::WORLD,
-                WC::ChatColor::YELLOW
-            );
-            return true;
-        }
-    }
-
-    return false;
-}
-
-static std::string GetDetectedTag(std::string const& msg)
-{
-    std::string lowerMsg = strToLower(msg);
-
-    if (g_wcConfig.tradeDetection)
-    {
-        if (ContainsAny(lowerMsg,
-            {
-                "wts", "wtb", "wtt", "lfw",
-                "selling", "buying", "for sale",
-                "enchanting", "jewelcrafting", "blacksmithing",
-                "alchemy", "tailoring", "leatherworking", "engineering"
-            }))
-        {
-            return g_wcConfig.tradeTag;
-        }
-    }
-
-    if (g_wcConfig.groupDetection)
-    {
-        if (ContainsAny(lowerMsg,
-            {
-                "lfg", "lfm", "looking for group", "looking for more",
-                "need tank", "need healer", "need heals", "need dps",
-                "tank needed", "healer needed", "dps needed",
-                "rdf", "heroic", "naxx", "ulduar", "toc", "icc", "voa", "os", "eoe"
-            }))
-        {
-            return g_wcConfig.groupTag;
-        }
-    }
-
-    return g_wcConfig.tag;
-}
-
-static std::string GetTagColor(std::string const& tag)
-{
-    if (tag == g_wcConfig.tradeTag)
-        return std::string(WC::ChatColor::TRADE);
-
-    if (tag == g_wcConfig.groupTag)
-        return std::string(WC::ChatColor::GROUP);
-
-    return std::string(WC::ChatColor::WORLD);
-}
-
-static bool IsIgnoredByTarget(Player* target, Player const& sender)
-{
-    if (!g_wcConfig.ignoreSupport)
-        return false;
-
-    if (!target || target == &sender)
-        return false;
-
-    PlayerSocial* social = target->GetSocial();
-    if (!social)
-        return false;
-
-    return social->HasIgnore(sender.GetGUID());
-}
-
 void WC::WorldChat_Config::OnBeforeConfigLoad(bool /*reload*/)
 {
     g_wcConfig.enabled = sConfigMgr->GetOption<bool>("WorldChat.Enable", true);
@@ -318,28 +107,10 @@ void WC::WorldChat_Config::OnBeforeConfigLoad(bool /*reload*/)
     g_wcConfig.loginState = sConfigMgr->GetOption<bool>("WorldChat.OnLogin.State", true);
     g_wcConfig.tag = sConfigMgr->GetOption<std::string>("WorldChat.Tag", "[World]");
     g_wcConfig.minimumLevel = sConfigMgr->GetOption<uint8>("WorldChat.MinimumLevel", 10);
-    g_wcConfig.ignoreSupport = sConfigMgr->GetOption<bool>("WorldChat.IgnoreSupport", true);
-
     g_wcConfig.floodWindowSeconds = sConfigMgr->GetOption<uint32>("WorldChat.Flood.WindowSeconds", 10);
     g_wcConfig.floodWarnCount = sConfigMgr->GetOption<uint32>("WorldChat.Flood.WarnCount", 3);
     g_wcConfig.floodMuteCount = sConfigMgr->GetOption<uint32>("WorldChat.Flood.MuteCount", 5);
     g_wcConfig.floodMuteSeconds = sConfigMgr->GetOption<uint32>("WorldChat.Flood.MuteSeconds", 60);
-
-    g_wcConfig.duplicateDetection = sConfigMgr->GetOption<bool>("WorldChat.DuplicateDetection", true);
-    g_wcConfig.duplicateWindowSeconds = sConfigMgr->GetOption<uint32>("WorldChat.Duplicate.WindowSeconds", 30);
-    g_wcConfig.duplicateMaxRepeats = sConfigMgr->GetOption<uint32>("WorldChat.Duplicate.MaxRepeats", 2);
-
-    g_wcConfig.urlFilter = sConfigMgr->GetOption<bool>("WorldChat.UrlFilter", true);
-    g_wcConfig.blockedUrlTerms = SplitCsv(sConfigMgr->GetOption<std::string>(
-        "WorldChat.UrlFilter.BlockedTerms",
-        "http://,https://,www.,.com,.net,.org,discord.gg,bit.ly,tinyurl"
-    ));
-
-    g_wcConfig.tradeDetection = sConfigMgr->GetOption<bool>("WorldChat.TradeDetection", true);
-    g_wcConfig.groupDetection = sConfigMgr->GetOption<bool>("WorldChat.GroupDetection", true);
-
-    g_wcConfig.tradeTag = sConfigMgr->GetOption<std::string>("WorldChat.TradeTag", "[Trade]");
-    g_wcConfig.groupTag = sConfigMgr->GetOption<std::string>("WorldChat.GroupTag", "[Group]");
 }
 
 void WC::SendWorldMessage(Player const& sender, std::string const& msg, int team)
@@ -349,7 +120,11 @@ void WC::SendWorldMessage(Player const& sender, std::string const& msg, int team
 
     if (!g_wcConfig.enabled)
     {
-        ChatHandler(sender.GetSession()).PSendSysMessage("{}[World]|r {}World Chat is disabled.|r", ChatColor::WORLD, ChatColor::RED);
+        ChatHandler(sender.GetSession()).PSendSysMessage(
+            "{}[World]|r {}World Chat is disabled.|r",
+            ChatColor::WORLD,
+            ChatColor::RED
+        );
         return;
     }
 
@@ -366,25 +141,19 @@ void WC::SendWorldMessage(Player const& sender, std::string const& msg, int team
 
     if (!sender.CanSpeak())
     {
-        ChatHandler(sender.GetSession()).PSendSysMessage("{}[World]|r {}You cannot use World Chat while muted.|r", ChatColor::WORLD, ChatColor::RED);
+        ChatHandler(sender.GetSession()).PSendSysMessage(
+            "{}[World]|r {}You cannot use World Chat while muted.|r",
+            ChatColor::WORLD,
+            ChatColor::RED
+        );
         return;
     }
-
-    if (ContainsBlockedUrl(sender, msg))
-        return;
-
-    if (IsDuplicateMessage(sender, msg))
-        return;
 
     if (IsFlooding(sender))
         return;
 
     AccountTypes senderSecurity = sender.GetSession()->GetSecurity();
     std::string senderName = sender.GetName();
-
-    std::string displayTag = GetDetectedTag(msg);
-    std::string tagColor = GetTagColor(displayTag);
-
     std::string audienceTag;
 
     if (team == TEAM_HORDE)
@@ -394,13 +163,13 @@ void WC::SendWorldMessage(Player const& sender, std::string const& msg, int team
 
     std::string outMessage;
 
-    // The original msg is inserted untouched to preserve WoW hyperlinks.
+    // The original msg is inserted untouched so WoW hyperlinks remain clickable.
     if (sender.isGMChat() || sender.IsDeveloper())
     {
         outMessage = Acore::StringFormat(
             "{}{}|r{} {}{}|Hplayer:{}|h{}|h|r: {}{}|r",
-            tagColor,
-            displayTag,
+            ChatColor::WORLD,
+            g_wcConfig.tag,
             audienceTag,
             GMIcon,
             ClassColor[sender.getClass()],
@@ -414,8 +183,8 @@ void WC::SendWorldMessage(Player const& sender, std::string const& msg, int team
     {
         outMessage = Acore::StringFormat(
             "{}{}|r{} {}|Hplayer:{}|h{}|h|r: {}{}|r",
-            tagColor,
-            displayTag,
+            ChatColor::WORLD,
+            g_wcConfig.tag,
             audienceTag,
             ClassColor[sender.getClass()],
             senderName,
@@ -427,8 +196,12 @@ void WC::SendWorldMessage(Player const& sender, std::string const& msg, int team
 
     WorldSessionMgr::SessionMap sessions = sWorldSessionMgr->GetAllSessions();
 
-    for (WorldSession* const& session : sessions | std::views::values)
+    // Deliberately avoid std::views::values here. Direct map iteration is
+    // compatible with both MSVC and GCC/Clang using older libstdc++ versions.
+    for (auto const& sessionEntry : sessions)
     {
+        WorldSession* session = sessionEntry.second;
+
         if (!session)
             continue;
 
@@ -442,44 +215,39 @@ void WC::SendWorldMessage(Player const& sender, std::string const& msg, int team
         if (!targetState.receiveEnabled)
             continue;
 
-        // Staff always receive faction-restricted World Chat messages so they can monitor both factions.
-        if (team != -1 &&
-            target->GetTeamId() != team &&
-            target->GetSession()->GetSecurity() == SEC_PLAYER)
-        {
+        if (team != -1 && target->GetTeamId() != team)
             continue;
-        }
 
         if (sender.GetTeamId() != target->GetTeamId()
             && !g_wcConfig.crossFaction
             && senderSecurity == SEC_PLAYER
             && target->GetSession()->GetSecurity() == SEC_PLAYER)
+        {
             continue;
-
-        if (IsIgnoredByTarget(target, sender))
-            continue;
+        }
 
         ChatHandler(target->GetSession()).PSendSysMessage(outMessage);
     }
 }
 
-static void SendSystemTaggedMessage(std::string const& tag, std::string const& tagColor, std::string const& msg, int team, bool gmOnly)
+void WC::SendWorldAnnouncement(std::string const& msg)
 {
     if (msg.empty())
         return;
 
     std::string outMessage = Acore::StringFormat(
-        "{}{}|r {}{}|r",
-        tagColor,
-        tag,
-        WC::ChatColor::TEXT,
+        "{}[Announcement]|r {}{}|r",
+        ChatColor::ANNOUNCE,
+        ChatColor::TEXT,
         msg
     );
 
     WorldSessionMgr::SessionMap sessions = sWorldSessionMgr->GetAllSessions();
 
-    for (WorldSession* const& session : sessions | std::views::values)
+    for (auto const& sessionEntry : sessions)
     {
+        WorldSession* session = sessionEntry.second;
+
         if (!session)
             continue;
 
@@ -488,147 +256,8 @@ static void SendSystemTaggedMessage(std::string const& tag, std::string const& t
         if (!target || !target->IsInWorld())
             continue;
 
-        if (gmOnly && session->GetSecurity() == SEC_PLAYER)
-            continue;
-
-        // Staff always receive faction-restricted API messages too.
-        if (team != -1 &&
-            target->GetTeamId() != team &&
-            session->GetSecurity() == SEC_PLAYER)
-        {
-            continue;
-        }
-
         ChatHandler(target->GetSession()).PSendSysMessage(outMessage);
     }
-}
-
-static void GetApiTagAndColor(WC::MessageChannel channel, std::string& tag, std::string& color, int& team, bool& gmOnly)
-{
-    tag = g_wcConfig.tag;
-    color = std::string(WC::ChatColor::WORLD);
-    team = -1;
-    gmOnly = false;
-
-    switch (channel)
-    {
-        case WC::MessageChannel::Announcement:
-            tag = "[Announcement]";
-            color = std::string(WC::ChatColor::ANNOUNCE);
-            break;
-
-        case WC::MessageChannel::Trade:
-            tag = g_wcConfig.tradeTag;
-            color = std::string(WC::ChatColor::TRADE);
-            break;
-
-        case WC::MessageChannel::Group:
-            tag = g_wcConfig.groupTag;
-            color = std::string(WC::ChatColor::GROUP);
-            break;
-
-        case WC::MessageChannel::Horde:
-            tag = g_wcConfig.tag + " |cffff2020[Horde Only]|r";
-            team = TEAM_HORDE;
-            break;
-
-        case WC::MessageChannel::Alliance:
-            tag = g_wcConfig.tag + " |cff3399ff[Alliance Only]|r";
-            team = TEAM_ALLIANCE;
-            break;
-
-        case WC::MessageChannel::GM:
-            tag = "[GM]";
-            color = std::string(WC::ChatColor::ANNOUNCE);
-            gmOnly = true;
-            break;
-
-        case WC::MessageChannel::World:
-        default:
-            break;
-    }
-}
-
-void WC::SendWorldAnnouncement(std::string const& msg)
-{
-    SendSystemTaggedMessage("[Announcement]", std::string(ChatColor::ANNOUNCE), msg, -1, false);
-}
-
-void WC::Broadcast(std::string const& msg)
-{
-    SendSystemTaggedMessage(g_wcConfig.tag, std::string(ChatColor::WORLD), msg, -1, false);
-}
-
-void WC::BroadcastFaction(std::string const& msg, int team)
-{
-    SendSystemTaggedMessage(g_wcConfig.tag, std::string(ChatColor::WORLD), msg, team, false);
-}
-
-void WC::BroadcastTrade(std::string const& msg)
-{
-    SendSystemTaggedMessage(g_wcConfig.tradeTag, std::string(ChatColor::TRADE), msg, -1, false);
-}
-
-void WC::BroadcastGroup(std::string const& msg)
-{
-    SendSystemTaggedMessage(g_wcConfig.groupTag, std::string(ChatColor::GROUP), msg, -1, false);
-}
-
-void WC::Broadcast(MessageChannel channel, std::string const& msg)
-{
-    std::string tag;
-    std::string color;
-    int team;
-    bool gmOnly;
-
-    GetApiTagAndColor(channel, tag, color, team, gmOnly);
-    SendSystemTaggedMessage(tag, color, msg, team, gmOnly);
-}
-
-void WC::SendToPlayer(Player* player, MessageChannel channel, std::string const& msg)
-{
-    if (!player || !player->IsInWorld() || msg.empty())
-        return;
-
-    std::string tag;
-    std::string color;
-    int team;
-    bool gmOnly;
-
-    GetApiTagAndColor(channel, tag, color, team, gmOnly);
-
-    if (gmOnly && player->GetSession()->GetSecurity() == SEC_PLAYER)
-        return;
-
-    ChatHandler(player->GetSession()).PSendSysMessage(
-        "{}{}|r {}{}|r",
-        color,
-        tag,
-        WC::ChatColor::TEXT,
-        msg
-    );
-}
-
-void WC::SendToFaction(uint32 teamId, MessageChannel channel, std::string const& msg)
-{
-    std::string tag;
-    std::string color;
-    int ignoredTeam;
-    bool gmOnly;
-
-    GetApiTagAndColor(channel, tag, color, ignoredTeam, gmOnly);
-    SendSystemTaggedMessage(tag, color, msg, teamId, gmOnly);
-}
-
-void WC::SendToGMs(MessageChannel channel, std::string const& msg)
-{
-    std::string tag;
-    std::string color;
-    int team;
-    bool gmOnly;
-
-    GetApiTagAndColor(channel, tag, color, team, gmOnly);
-    SendSystemTaggedMessage(tag, color, msg, -1, true);
 }
 
 void WC::WorldChat_Announce::OnPlayerLogin(Player* player)
@@ -665,12 +294,19 @@ bool WC::WorldChat_Announce::OnPlayerCanUseChat(Player* player, uint32 type, uin
     return false;
 }
 
-bool WC::WorldChat_Announce::OnPlayerCanUseChat(Player* player, uint32 /*type*/, uint32 lang, std::string& msg, Channel* channel)
+bool WC::WorldChat_Announce::OnPlayerCanUseChat(
+    Player* player,
+    uint32 /*type*/,
+    uint32 lang,
+    std::string& msg,
+    Channel* channel)
 {
     if (!player || !channel)
         return true;
 
-    if (g_wcConfig.channelName != "" && lang != LANG_ADDON && strToLower(channel->GetName()) == g_wcConfig.channelName)
+    if (g_wcConfig.channelName != ""
+        && lang != LANG_ADDON
+        && strToLower(channel->GetName()) == g_wcConfig.channelName)
     {
         SendWorldMessage(*player, msg, -1);
         return false;
@@ -713,7 +349,11 @@ bool WC::WorldChat::HandleWorldChatOnCommand(ChatHandler* handler)
     state.receiveEnabled = true;
     SavePlayerState(player);
 
-    handler->PSendSysMessage("{}[World]|r {}World Chat is now visible.|r", ChatColor::WORLD, ChatColor::GREEN);
+    handler->PSendSysMessage(
+        "{}[World]|r {}World Chat is now visible.|r",
+        ChatColor::WORLD,
+        ChatColor::GREEN
+    );
     return true;
 }
 
@@ -726,7 +366,11 @@ bool WC::WorldChat::HandleWorldChatOffCommand(ChatHandler* handler)
     state.worldMode = false;
     SavePlayerState(player);
 
-    handler->PSendSysMessage("{}[World]|r {}World Chat is now hidden.|r", ChatColor::WORLD, ChatColor::YELLOW);
+    handler->PSendSysMessage(
+        "{}[World]|r {}World Chat is now hidden.|r",
+        ChatColor::WORLD,
+        ChatColor::YELLOW
+    );
     return true;
 }
 
@@ -786,7 +430,6 @@ Acore::ChatCommands::ChatCommandTable WC::WorldChat::GetCommands() const
     static Acore::ChatCommands::ChatCommandTable commandTable =
     {
         { "world", worldCommandTable },
-
         { "worldgm", HandleWorldChatAnnounceCommand, SEC_MODERATOR, Acore::ChatCommands::Console::No },
         { "worldh", HandleWorldChatHordeCommand, SEC_MODERATOR, Acore::ChatCommands::Console::No },
         { "worlda", HandleWorldChatAllianceCommand, SEC_MODERATOR, Acore::ChatCommands::Console::No },
